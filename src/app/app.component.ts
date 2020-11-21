50eb7266497b1aa4c91ab1cfe2ff87791afdb152
Adrian Krasowski <ADRIAN433@bailliegifford.com>
2020-11-21 20:55:54 +0100

    Start page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PFR';
+  public isInitialized: boolean = false;
+
+  public initializePage() {
+    this.isInitialized = true;
+  }
 }
